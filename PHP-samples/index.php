e097fc0435bb59224ec7b69690291949df7e4331
Berjooj <moreira.bernardo@yahoo.com.br>
2021-03-08 17:16:48 -0300

    questão 4

--- PHP-samples/index.php
+++ PHP-samples/index.php
@@ -1,3 +1,32 @@
 <?php
-    echo "Hello world";
+    $variavel = 198;
+
+    intval($variavel);
+
+    if (!empty($variavel) && $variavel == 198) {
+        echo "Variável é válida<br>";
+    } else {
+        echo "Variável inválida!";
+    }
+
+    //--------------------------------------------
+
+    $array = [ "nome" => "Pedro Luiz", "endereco" => "Av Taquara", "telefone" => "(51) 5151-51511" ];
+
+    if (!empty($array["endereco"])) { 
+        echo $array["endereco"] . "<br>";
+    } else {
+        echo "Endereço não encontrado!<br>";
+    }
+
+    //--------------------------------------------
+    
+    $nome = "Romarinho";
+
+    if (!empty($nome) && strcmp($nome, "Romarinho")==0) {
+        echo $nome . "<br>";
+    } else {
+        echo "Variável inválida!";
+    }
+    
 ?>
